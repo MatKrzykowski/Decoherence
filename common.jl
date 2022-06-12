367114c327960e52a2758769d718ae2c5417450b
MKrzykow <matkrzykowski@gmail.com>
2022-06-12 18:42:27 +0200

    Fix structure of common module

--- common.jl
+++ common.jl
@@ -1,46 +1,46 @@
-module common
+module Common
 
-    using PhysicalConstants.CODATA2018
-    using Unitful
+using PhysicalConstants.CODATA2018
+using Unitful
 
-    ############
-    # Matrices #
-    ############
+############
+# Matrices #
+############
 
-    # Function calculating commutator or two matrices
-    function commutator(A::AbstractMatrix, B::AbstractMatrix)::AbstractMatrix
-        A * B - B * A
+# Function calculating commutator or two matrices
+function commutator(A::AbstractMatrix, B::AbstractMatrix)::AbstractMatrix
+    A * B - B * A
+end
+
+# Pauli matrices
+σ_x = [0.0 1.0
+    1.0 0.0] / 2
+σ_y = [0.0 -1.0im
+    1.0im 0.0] / 2
+σ_z = [1.0 0.0
+    0.0 -1.0] / 2
+
+######################
+# Physical constants #
+######################
+
+ħ = PlanckConstant / 2 / π # Reduced planck constant
+μ_B = BohrMagneton # Bohr magneton
+k_B = BoltzmannConstant # Boltzmann constant
+
+############################
+# Bose-Einstein statistics #
+############################
+
+function n_B(ω::Quantity{Float64, Unitful.𝐓^-1},
+            T::Quantity{Float64, Unitful.𝚯}
+            )::Float64
+    if ω == 0u"Hz"
+        return 0
+    elseif T == 0u"K"
+        return ω > 0u"Hz" ? 1.0 : 0.0
     end
-
-    # Pauli matrices
-    σ_x = [0.0 1.0
-        1.0 0.0] / 2
-    σ_y = [0.0 -1.0im
-        1.0im 0.0] / 2
-    σ_z = [1.0 0.0
-        0.0 -1.0] / 2
-
-    ######################
-    # Physical constants #
-    ######################
-
-    ħ = PlanckConstant / 2 / π # Reduced planck constant
-    μ_B = BohrMagneton # Bohr magneton
-    k_B = BoltzmannConstant # Boltzmann constant
-
-    ############################
-    # Bose-Einstein statistics #
-    ############################
-
-    function n_B(ω::Quantity{Float64, Unitful.𝐓^-1},
-                T::Quantity{Float64, Unitful.𝚯}
-                )::Float64
-        if ω == 0u"Hz"
-            return 0
-        elseif T == 0u"K"
-            return ω > 0u"Hz" ? 1.0 : 0.0
-        end
-        return abs(1.0 / expm1(ħ * ω / (k_B * T)) + 1.0)
-    end
+    return abs(1.0 / expm1(ħ * ω / (k_B * T)) + 1.0)
+end
 
 end
