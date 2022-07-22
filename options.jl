b306c050484b0d4654eb57c78c2455c6c69fa1e2
MKrzykow <matkrzykowski@gmail.com>
2022-07-22 18:56:15 +0200

    Add options to options

--- options.jl
+++ options.jl
@@ -5,6 +5,8 @@ using Parameters
 @with_kw struct Ops
     carrier::Char = 'h'
     n_states::Int64 = 4
+    ODE_rtol::Float64 = 1e-8
+    output_filename::String = "output.csv"
 
     is_hole::Function = () -> carrier == 'h'
     is_electron::Function = () -> carrier == 'e'
