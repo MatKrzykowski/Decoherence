376d27571f4756ef4d8055a047e34c59cd97a7a6
MKrzykow <matkrzykowski@gmail.com>
2022-07-22 20:57:26 +0200

    Further experiments with ODE

--- ode.jl
+++ ode.jl
@@ -10,29 +10,25 @@ u0[1, 1] = 1.0
 u0
 
 function f(ρ, p, t)
-    H, time_unit = p
-    -1im / ħ * commutator(H, ρ) * time_unit
+    H = p
+    result = -1im / ħ * commutator(H, ρ)
+    result * 1u"s"
 end
 
 B = 1u"T"
-H = B * μ_B * kron(σ_0, σ_x)
-f(u0, (H, 0), 0)
--1im / ħ * commutator(H, u0)
-# f(u, p, t) = 0.98u
-# u0 = 1.0
+H = Hermitian(B * μ_B * kron(σ_0, σ_x))
 
-params = (H, time_unit)
-tspan = (0.0u"s", 500u"ps")
-prob = ODEProblem(f, u0, tspan ./ time_unit, params)
+# display(f((H), u0, 0.0))
+
+params = (H)
+tspan = (0.0u"s", 1.0u"ns") ./ 1u"s"
+prob = ODEProblem(f, u0, tspan, params)
 sol = solve(prob, abstol=1e-9, reltol=1e-9)
 sol
-
-# x = [x[1, 1] for x in sol.u]
-
-
 
 using Plots
 gr()
 
 plot(sol.t, real([x[1, 1] for x in sol.u]))
 plot!(sol.t, real([x[2, 2] for x in sol.u]))
+plot!(sol.t, real([tr(x) for x in sol.u]), label="trace")
